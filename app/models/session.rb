ebdacb723a0ca3c3b073419e7f09af0290ee5246
Mike Goggin <michael@mgoggin.com>
2024-03-05 23:08:56 -0600

    chore(dev): Annotate `Session`

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: sessions
+#
+#  id         :bigint           not null, primary key
+#  ip_address :string
+#  user_agent :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  player_id  :bigint           not null
+#
+# Indexes
+#
+#  index_sessions_on_player_id  (player_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (player_id => players.id)
+#
 class Session < ApplicationRecord
   belongs_to :player
 
